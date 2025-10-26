a39adef5d992667afd5b4150621f1c65aaeb9308
Keegan Brown <keeganbrown@outlook.de>
2025-10-26 08:05:55 +0100

    feat: add flash highlighting in main search

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -1,12 +1,21 @@
+-- https://github.com/folke/flash.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
 return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  ---@type Flash.Config
-  opts = {},
-  -- stylua: ignore
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {
+        modes = {
+            search = {
+                enabled = true,
+                highlight = { backdrop = true, },
+            }
+        },
+        prompt = {
+            enabled = false,
+        },
+    },
+    keys = {
+        { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,   desc = "Flash" },
+        { "r",     mode = "o",               function() require("flash").remote() end, desc = "Remote Flash" },
+        { "<c-s>", mode = { "c" },           function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
 }
