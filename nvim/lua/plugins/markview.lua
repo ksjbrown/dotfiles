5dafde9ed366f7715b021f2079d0457ed1f125e9
ksj <keeganbrown@outlook.de>
2026-05-05 15:52:49 +0200

    nvim: fix markview codecompanion integration

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -3,8 +3,10 @@ return {
     "OXY2DEV/markview.nvim",
     lazy = false,
     dependencies = { "saghen/blink.cmp" },
-    preview = {
-        filetypes = { "markdown", "codecompanion" },
-        ignore_buftypes = {},
-    },
-};
+    opts = {
+        preview = {
+            filetypes = { "markdown", "codecompanion" },
+            ignore_buftypes = {},
+        },
+    }
+}
