4daccf776329115241d3337d6d346ccd8e70227a
Keegan Brown <keeganbrown@outlook.de>
2026-05-05 21:28:34 +0200

    nvim: slightly better codecompanion window split

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -3,7 +3,7 @@ return {
     version = "^19.0.0",
     keys = {
         { "<leader>a", function() require("codecompanion").toggle({ window_opts = { layout = "float", width = 0.8, height = 0.8 } }) end },
-        { "<leader>A", function() require("codecompanion").toggle({ window_opts = { split = "right" } }) end },
+        { "<leader>A", function() require("codecompanion").toggle({ window_opts = { split = "right", width = 0.33, } }) end },
     },
     dependencies = {
         "nvim-lua/plenary.nvim",
