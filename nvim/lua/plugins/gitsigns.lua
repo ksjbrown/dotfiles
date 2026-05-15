7570d6a1c23a1eeb0a67ad851816e37e7ef1dc78
ksj <keeganbrown@outlook.de>
2026-05-15 09:49:34 +0200

    wip

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,7 +1,15 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = "BufEnter",
+	keys = {
+		{ "<leader>gb", "<Cmd>Gitsigns toggle_current_line_blame<CR>", mode = { "n", "v" } },
+		{ "<leader>gd", "<Cmd>Gitsigns diffthis<CR>", mode = { "n", "v" } },
+		{ "<leader>gw", "<Cmd>Gitsigns toggle_word_diff<CR>", mode = { "n", "v" } },
+	},
 	opts = {
 		attach_to_untracked = true,
+        current_line_blame_opts = {
+            delay = 0,
+        },
 	},
 }
