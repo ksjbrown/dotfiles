8f38415a836cf9f175aed27e1af31a0624125c23
ksj <keeganbrown@outlook.de>
2026-05-18 00:33:22 +0200

    nvim: better signature display behaviour

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -35,7 +35,18 @@ return {
         },
         keymap = {
             preset = "default",
+            ["<Enter>"] = {
+                -- accept selection only if completion menu is shown
+                function(cmp)
+                    if cmp.is_menu_visible() then
+                        cmp.accept()
+                        return true
+                    end
+                end,
+                "fallback",
+            },
             ["<Esc>"] = {
+                -- close complete menu, but stay in insert mode
                 function(cmp)
                     if cmp.is_menu_visible() then
                         cmp.hide()
@@ -45,34 +56,11 @@ return {
                 "fallback",
             },
             ["<Tab>"] = { "accept", "fallback" },
-            ["<Enter>"] = {
-                function(cmp)
-                    if cmp.is_menu_visible() then
-                        cmp.accept()
-                        return true
-                    end
-                end,
-                "fallback",
-            },
-            ["<C-k>"] = {
-                function(cmp)
-                    if not cmp.is_menu_visible() then
-                        return
-                    end
-                    if not cmp.is_documentation_visible() then
-                        cmp.show_documentation()
-                    else
-                        cmp.hide_documentation()
-                    end
-                    return true
-                end,
-                "fallback",
-            },
         },
         signature = {
             enabled = true,
-            window = {
-                show_documentation = false,
+            trigger = {
+                enabled = false,
             },
         },
         sources = {
